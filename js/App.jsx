556ce04d918036e498bf32c92b858d70f8953c98
Nicolas Surian <nicolas.surian@gmail.com>
2018-07-17 09:19:16 +0200

    Revert Last Commit

--- js/App.jsx
+++ js/App.jsx
@@ -3,8 +3,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import type { Match } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
 import Landing from './Landing';
 import Search from './Search';
 import Details from './Details';
@@ -14,22 +12,20 @@ const FourOhFour = () => <h1>Oops ! 404</h1>;
 
 const App = () => (
   <BrowserRouter>
-    <Provider store={store}>
-      <div className="app">
-        <Switch>
-          <Route exact path="/" component={props => <Landing shows={preload.shows} {...props} />} />
-          <Route path="/search" component={props => <Search shows={preload.shows} {...props} />} />
-          <Route
-            path="/details/:id"
-            component={(props: { match: Match }) => {
-              const selectedShow = preload.shows.find(show => props.match.params.id === show.imdbID);
-              return <Details show={selectedShow} {...props} />;
-            }}
-          />
-          <Route component={FourOhFour} />
-        </Switch>
-      </div>
-    </Provider>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={props => <Landing shows={preload.shows} {...props} />} />
+        <Route path="/search" component={props => <Search shows={preload.shows} {...props} />} />
+        <Route
+          path="/details/:id"
+          component={(props: { match: Match }) => {
+            const selectedShow = preload.shows.find(show => props.match.params.id === show.imdbID);
+            return <Details show={selectedShow} {...props} />;
+          }}
+        />
+        <Route component={FourOhFour} />
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 
