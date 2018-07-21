ea013e63b14d1704c258aef5d3581eda7023ef00
Nicolas Surian <nicolas.surian@gmail.com>
2018-07-21 10:39:08 +0200

    9.8 Using Redux in the Search Component

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,45 +1,26 @@
 // @flow
 
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import ShowCard from './ShowCard';
 import Header from './Header';
 
-class Search extends Component {
-  static defaultProps = {
-    search: ''
-  };
-  state = {
-    searchTerm: '',
-    version: 5,
-    isDone: true
-  };
-  componentWillMount() {
-    if (this.props.search) {
-      this.setState({ searchTerm: this.props.search });
-    }
-  }
-  props: {
-    shows: Array<Show>,
-    search?: string
-  };
-  handleSearchTermChange = (event: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
-    this.setState({ searchTerm: event.target.value });
-  };
-  render() {
-    return (
-      <div className="search">
-        <Header showSearch searchTerm={this.state.searchTerm} handleSearchTermChange={this.handleSearchTermChange} />
-        <div>
-          {this.props.shows
-            .filter(
-              show =>
-                `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
-            )
-            .map(show => <ShowCard {...show} key={show.imdbID} />)}
-        </div>
-      </div>
-    );
-  }
-}
+const Search = (props: {
+  searchTerm: string, // eslint-disable-line react/no-unused-prop-types
+  shows: Array<Show>
+}) => (
+  <div className="search">
+    <Header showSearch />
+    <div>
+      {props.shows
+        .filter(show => `${show.title} ${show.description}`.toUpperCase().indexOf(props.searchTerm.toUpperCase()) >= 0)
+        .map(show => <ShowCard {...show} key={show.imdbID} />)}
+    </div>
+  </div>
+);
 
-export default Search;
+const mapStateToProps = state => ({
+  searchTerm: state.searchTerm
+});
+
+export default connect(mapStateToProps)(Search);
