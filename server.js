a800d78c84f6362354a1aa0a20c2c0f62db267ff
Nicolas Surian <nicolas.surian@gmail.com>
2018-07-23 20:53:33 +0200

    12.5 Fixing HMR

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const ReactDOMServer = require('react-dom/server');
 const ReactRouter = require('react-router-dom');
 const _ = require('lodash');
 const fs = require('fs');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const webpack = require('webpack');
+const config = require('./webpack.config')
 const App = require('./js/App').default;
 
 const StaticRouter = ReactRouter.StaticRouter;
@@ -15,6 +19,14 @@ const baseTemplate = fs.readFileSync('./index.html');
 const template = _.template(baseTemplate);
 
 const server = express();
+
+const compiler = webpack(config);
+server.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath
+  })
+);
+server.use(webpackHotMiddleware(compiler));
 
 server.use('/public', express.static('./public'));
 
