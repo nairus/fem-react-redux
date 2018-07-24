37adb704d2414e298e528e3c26d41667a86f5c7d
Nicolas Surian <nicolas.surian@gmail.com>
2018-07-24 13:34:39 +0200

    13.4 Creating a Landing Bundle

--- js/App.jsx
+++ js/App.jsx
@@ -5,7 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 import type { Match } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import Landing from './Landing';
+import AsyncRoute from './AsyncRoute';
 import Search from './Search';
 import Details from './Details';
 import preload from '../data.json';
@@ -16,7 +16,7 @@ const App = () => (
   <Provider store={store}>
     <div className="app">
       <Switch>
-        <Route exact path="/" component={Landing} />
+        <Route exact path="/" component={props => <AsyncRoute props={props} loadingPromise={import('./Landing')} />} />
         <Route path="/search" component={props => <Search shows={preload.shows} {...props} />} />
         <Route
           path="/details/:id"
