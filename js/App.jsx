a63fcc87bf159e18ed64014dc1e14fc86a44238d
Nicolas Surian <nicolas.surian@gmail.com>
2018-07-25 06:41:57 +0200

    13.5 Creating Search and Details Bundles

--- js/App.jsx
+++ js/App.jsx
@@ -6,8 +6,6 @@ import type { Match } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import AsyncRoute from './AsyncRoute';
-import Search from './Search';
-import Details from './Details';
 import preload from '../data.json';
 
 const FourOhFour = () => <h1>Oops ! 404</h1>;
@@ -17,12 +15,22 @@ const App = () => (
     <div className="app">
       <Switch>
         <Route exact path="/" component={props => <AsyncRoute props={props} loadingPromise={import('./Landing')} />} />
-        <Route path="/search" component={props => <Search shows={preload.shows} {...props} />} />
+        <Route
+          path="/search"
+          component={props => (
+            <AsyncRoute props={Object.assign({ shows: preload.shows }, props)} loadingPromise={import('./Search')} />
+          )}
+        />
         <Route
           path="/details/:id"
           component={(props: { match: Match }) => {
             const selectedShow = preload.shows.find(show => props.match.params.id === show.imdbID);
-            return <Details show={selectedShow} {...props} />;
+            return (
+              <AsyncRoute
+                props={Object.assign({ show: selectedShow, match: {}, props })}
+                loadingPromise={import('./Details')}
+              />
+            );
           }}
         />
         <Route component={FourOhFour} />
